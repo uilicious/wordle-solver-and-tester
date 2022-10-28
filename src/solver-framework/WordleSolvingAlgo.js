6be1b6a6a40c970d934697593a130795966a2169
Eugene Cheah <eugeneqin@gmail.com>
2022-10-28 11:37:43 +0000

    more tuning!

--- src/solver-framework/WordleSolvingAlgo.js
+++ src/solver-framework/WordleSolvingAlgo.js
@@ -438,7 +438,7 @@ class WordleSolvingAlgo {
 		// The default scoring function is below ....
 		//----------------------------------------------------
 		
-		this.rankingFunction = require("../ranking-function/v3-weightedScore");
+		this.rankingFunction = require("../ranking-function/v3-stageAwareness");
 		return this.rankingFunction(word, charStats, state);
 	}
 }
